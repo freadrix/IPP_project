ea49c5ad410178704b0765b89575c3559e87e068
Anton Medvedev <amedvede@redhat.com>
2022-03-14 17:25:21 +0100

    feat: project todo

---
+++ parse.php
@@ -0,0 +1,13 @@
+<?php
+// introduce myself (hlavicka)
+// TODO napsat funkce ktera vypise pomocne hlaseni pomoce "echo"
+// TODO vyresit kolizi argumentu
+// TODO nacist stdin
+// TODO najit zacatek programu
+// TODO udelat regex na bile znaky prazdny radky
+// TODO napsat ignorovani komentaru
+// TODO najit keywords (instrukce)
+// TODO funkce na vypisani xml
+// TODO funkce na check argumentu po prikazu
+
+?>
